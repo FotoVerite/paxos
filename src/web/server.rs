f728f3f83537129c003a67589dda69278d544517
Matthew Bergman <mzbphoto@gmail.com>
2026-01-14 16:27:27 -0500

    started polishing

--- src/web/server.rs
+++ src/web/server.rs
@@ -19,7 +19,7 @@ pub async fn run_web_server() {
     let app = Router::new()
         .route("/", get(landing_handler))
         .route("/whitepapers", get(whitepapers_handler))
-        .route("/leslie", get(leslie_handler))
+        .route("/leslie-lamport-biography", get(leslie_handler))
         .route("/visualizer", get(visualizer_handler))
         .route("/senate", get(senate_handler))
         .route("/setting_of_paper", get(setting_of_paper_handler))
